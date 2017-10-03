8feca4fb045f29c3401a81b981d99e980f1c2bae
Thorsten <thorsten.stoffregen@gmx.de>
2017-10-03 22:45:09 +0200

    Added base plugin class.

---
+++ plugins/plugin.py
@@ -0,0 +1,63 @@
+
+class PluginBase(object):
+    """
+    Base for all Plugins
+    """
+    
+    STATUS_DISABLED = "disabled"
+    STATUS_OK = "ok"
+    STATUS_WARNING = "warning"
+    STATUS_ERROR = "error"
+
+    global_id = 0 # Unique ID for all plugin-instances.
+    
+    def __init__(self, config = None):
+        """
+        Inits a new instance of the plugins.
+        If config is given, the values are loaded. Otherwise default parameters.
+        """
+        self.status = self.STATUS_OK
+        self.id = self.global_id
+        self.global_id += 1
+        
+        if config is None:
+            self.preferences = [Preferences(name="enabled", 
+                                            param_type=bool, 
+                                            value = True)]
+        else:
+            pass
+    
+    def on_startup(self):
+        """
+        Called on Application startup.
+        """
+        pass
+    
+    def on_run(self):
+        """
+        Called to run the plugin. 
+        """
+        pass
+    
+    def on_prepare_run(self):
+        """
+        Called when a new run is started.
+        """
+        pass
+    
+class Preferences(object):
+    """
+    Class for Plugin-Preferences.
+    
+    Contains:
+        id: Unqiue ID for all Objects.
+        name: Name of Parameter.
+        type: Type of parameter (int, str, float, bool).
+        default: Default Value if not set.
+        value: Aktual value of parameter.
+    """
+    def __init__(self, name, param_type, default, value=None):
+        self.name = name
+        self.param_type = param_type
+        self.default = default
+        self.value = value if value is not None else default
---
+++ test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 22:35:41 2017
+
+@author: dh9ts
+"""
+
+import unittest
+from plugins.plugin import PluginBase, Preferences
+
+class TestPlugin(unittest.TestCase):
+    pass
+
+
+class TestPreferences(unittest.TestCase):
+    def setUp(self):
+        self.pref = Preferences(name="nam", 
+                                param_type=int, 
+                                default=12, 
+                                value=None)
+        
+    def test_name(self):
+        self.assertEqual(self.pref.name, "nam")
+        
+    def test_type(self):
+        self.assertEqual(self.pref.param_type, int)
+        
+    def test_default(self):
+        self.assertEqual(self.pref.default, 12)
+        
+    def test_default_value(self):
+        self.assertEqual(self.pref.value, 12)
+        
+class TestPreferencesValue(unittest.TestCase):
+    def setUp(self):
+        self.pref = Preferences(name="nam", 
+                                param_type=int, 
+                                default=12, 
+                                value=13)
+
+    def test_set_value(self):
+        
+        self.assertEqual(self.pref.value, 13)
+
+if __name__ == '__main__':
+    unittest.main()
